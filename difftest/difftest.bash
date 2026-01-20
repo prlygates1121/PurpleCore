e721ab23deccf98836f6e779f3c2b799199c367d
prlygates1121 <3095901180@qq.com>
2026-01-21 03:35:07 +0800

    Update ISA to rv32i_zicsr in scripts and Makefile
    Changed the ISA from rv32im_zicsr/rv32imzicsr to rv32i_zicsr/rv32izicsr in difftest.bash and Makefile, now that we don't have multiplication or division.

--- difftest/difftest.bash
+++ difftest/difftest.bash
@@ -5,4 +5,4 @@ fi
 
 LINE_COUNT=$(($(wc -l < "vivado_dut.log") + 6))
 
-spike --isa=rv32im_zicsr --pc=0x80000200 -m0x80000000:0xC000 --instructions=$LINE_COUNT --log-commits ../program/output/my_program 2> spike.log 
+spike --isa=rv32i_zicsr --pc=0x80000200 -m0x80000000:0xC000 --instructions=$LINE_COUNT --log-commits ../program/output/my_program 2> spike.log 
